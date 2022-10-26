3f6b039db1ed46eb1202a19e194c0e0d65e12596
ShrutiAgarwalll <116299400+ShrutiAgarwalll@users.noreply.github.com>
2022-10-26 14:22:55 +0530

    Create Day6.cpp

---
+++ Day6.cpp
@@ -0,0 +1,25 @@
+//odd sum pair
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+    cin>>a;
+	for(int i=0; i<a; i++)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+        if(a%2!=0 and b%2!=0 and c%2!=0)
+	    {
+	        cout<<"NO";
+	    }
+	    else if(a%2!=0 || b%2!=0 || c%2!=0)
+	    {
+	        cout<<"YES";
+	    }
+        else
+        {
+            cout<<"NO";
+        }
+	}
+}
