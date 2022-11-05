3d17457e2c7e6a3ad5fbab13e5bf03eff6bf7510
ShrutiAgarwalll <116299400+ShrutiAgarwalll@users.noreply.github.com>
2022-11-05 17:43:41 +0530

    Create Day16.cpp

---
+++ Day16.cpp
@@ -0,0 +1,23 @@
+//second max of 3 numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int N,a,b,c;
+	cin>>N;
+	int i;
+	for(i=0;i<N;i++)
+	{
+	    cin>>a>>b>>c;
+	    if((a>b&&a<c)||(a>c&&a<b))
+	    cout<<a<<endl;
+	    else if((b>a&&b<c)||(b>c&&b<a))
+	    cout<<b<<endl;
+	    else 
+	    cout<<c<<endl;
+	}
+	return 0;
+	
+	
+}
