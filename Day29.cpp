a172eba8d1ac4399f9562d97288b53f200ff1151
ShrutiAgarwalll <116299400+ShrutiAgarwalll@users.noreply.github.com>
2022-11-18 20:47:43 +0530

    Create Day29.cpp

---
+++ Day29.cpp
@@ -0,0 +1,28 @@
+//police and thief
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    for(int i=0;i<a;i++)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        if(X>Y)
+        {
+            cout<<X-Y<<endl;
+        }
+        else if(X<Y)
+        {
+            cout<<Y-X<<endl;
+        }
+        else
+        {
+            cout<<"0"<<endl;
+        }
+            
+    }
+
+    return 0;
+}
